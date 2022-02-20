0616ad08d4054658eaac93e4fb745de89a51438a
Vlad <64969216+dreamkaman@users.noreply.github.com>
2022-02-20 23:36:46 +0200

    next

--- server.js
+++ server.js
@@ -1,5 +1,25 @@
-const app = require("./app");
+// const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000");
+// });
+
+const mongoose = require("mongoose");
+
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+// const DB_HOST =
+//   "mongodb+srv://Vlad:eKnFNsie8chkaUD@cluster0.u67sh.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+// const { DB_HOST } = require("./config");
+const { DB_HOST } = process.env;
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => console.log("Database connection successful"))
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
